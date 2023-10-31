9ce55b251d51fed3e12576ee16f01abac5c903d9
Odenfis <131205875+Yuri-Lib4@users.noreply.github.com>
2023-10-31 11:46:06 -0500

    forst

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+
+# Directorio de trabajo de Render
+RENDER_WORKING_DIR=support
+
+# Cambia al directorio de trabajo de Render
+cd $RENDER_WORKING_DIR
+# Instala las dependencias desde requirements.txt si existe
+if [ -f "requirements.txt" ]; then
+    pip install -r requirements.txt
+else
+    echo "El archivo requirements.txt no se encontró en la ubicación especificada.."
+    exit 1
+fi
+
+rm db.sqlite3
+rm -r esupport/migrations
+python manage.py collectstatic --no-input
+
+python manage.py migrate
